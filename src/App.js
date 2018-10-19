2dedbf10e81f64560ed5dabadaa54597b64f7b0e
Devon Norris <norris.devon@gmail.com>
2018-10-19 14:49:36 -0400

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router as BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Header from './Header';
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,2 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 
+const Home = () => (
+  <Container>
+
+  </Container>
+
+)
+
+export default Home;
