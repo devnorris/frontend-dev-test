36c2b13eddabf393ea500327913f9876c23098c8
Devon Norris <norris.devon@gmail.com>
2018-10-20 21:42:58 -0400

    Implement form popup and start work on the board

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { Component } from 'react';
+import styled from 'react-emotion';
 
-const Game = () => <div />;
+const Container = styled('div')`
+  text-align: center;
+  align-content: center;
+`;
+
+const StyledBox = styled('div')`
+  width: 150px;
+  height: 150px;
+  border: 5px solid #2d2a2a;
+  display: inline-block;
+  background-color: #525258;
+`;
+
+const Board = styled('div')`
+  display: inline-flex;
+  flex-wrap: wrap;
+  width: 450px;
+`;
+
+class Game extends Component {
+  state = {
+    board: Array(9).fill(null)
+  };
+
+  handleClick(index) {
+    console.log(index);
+  }
+
+  render() {
+    const Box = this.state.board.map((box, index) => (
+      <StyledBox key={index} onClick={() => this.handleClick(index)}>
+        {box}
+      </StyledBox>
+    ));
+    return (
+      <Container>
+        <Board>{Box}</Board>
+      </Container>
+    );
+  }
+}
 
 export default Game;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'react-emotion';
+import { Modal, Form } from 'semantic-ui-react';
+
 import '../App.css';
 
-const Home = () => (
-  <div>
-    <div className="home-logo" />
-    <button className="btn">New Game</button>
-  </div>
-);
+const Button = styled('button')`
+  width: 7rem;
+  background: #7ed0b5;
+  color: black;
+  border: 1px solid currentColor;
+  border-radius: 100px;
+  padding: 12px 6px;
+  outline: none;
+  cursor: pointer;
+  font-size: 1rem;
 
+  :hover {
+    transform: translateY(-3px);
+    box-shadow: 0 10px 20px rgba(199, 199, 199, 0.2);
+  }
+`;
+
+class Home extends Component {
+  state = {
+    showModal: false
+  };
+
+  handleSubmit = () => {
+    console.log(this.state.player1)
+  }
+
+  updateInputeValue = evt => {
+    this.setState({ player1: evt.target.value })
+  }
+  render() {
+    return (
+      <div>
+        <div className="home-logo" />
+        <Modal
+          trigger={
+            <Button onClick={() => this.setState({ showModal: true })}>
+              New Game
+            </Button>
+          }
+        >
+          <Form>
+            <Form.Field>
+              <label>Player 1</label>
+              <input type="text" onChange={evt => this.updateInputeValue(evt)} placeholder="Players Name" />
+            </Form.Field>
+            <Form.Field>
+              <label>Player 2</label>
+              <input placeholder="Players Name" />
+            </Form.Field>
+            <Modal.Actions>
+              <Button type="submit">Start a New Game</Button>
+            </Modal.Actions>
+          </Form>
+        </Modal>
+        <Button>Credits</Button>
+        <Button>Exit</Button>
+      </div>
+    );
+  }
+}
 export default Home;
